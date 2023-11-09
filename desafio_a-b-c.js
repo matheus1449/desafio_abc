ce3382a03a5c51da0837890186ab26d0c93210e6
Matheus Francisco <matheus.1449@outlook.com>
2023-11-09 14:54:42 -0300

    desafio do curso realizado

---
+++ desafio_a-b-c.js
@@ -0,0 +1,25 @@
+var a = 10
+var b = 20
+var c = null
+
+document.write('A: ' + a + '<br />')
+document.write('B: ' + b + '<br />')
+document.write('C: ' + c + '<br />')
+
+document.write('<hr />')
+
+
+
+//logica de inversao dos valores das variaveis A e B
+
+
+//
+c = a
+a = 20
+b = 10
+c = null
+
+document.write('A: ' + a + '<br />')
+document.write('B: ' + b + '<br />')
+document.write('C: ' + c + '<br />')
+
